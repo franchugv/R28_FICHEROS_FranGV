f280bb64baee6b0415ca2c076cded10068fe68d1
DAM 1 <DAM 1@DESKTOP-TKRSI9T>
2024-04-10 12:49:19 +0200

    Agregar archivos de proyecto.

---
+++ R28_FICHEROS_FranGV/APIFichero/APIFichero.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R28_FICHEROS_FranGV.APIFichero
+{
+    public static class APIFichero
+    {
+
+        // RECURSOS
+
+        private const string RUTA = "Nombres.txt";
+
+        public static void CrearFichero()
+        {
+            // Recursos
+
+            StreamWriter Escritor;
+
+            Escritor = File.CreateText(RUTA);
+
+            Escritor.Close();
+        }
+
+        public static void EscribirFichero(string nombre)
+        {
+            // Recursos
+
+            StreamWriter Escritor;
+           
+            
+            if (!File.Exists(RUTA)) CrearFichero();
+
+            File.AppendAllText(RUTA, $"{nombre}\ns");
+
+
+
+        }
+    }
+}
---
+++ R28_FICHEROS_FranGV/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace R28_FICHEROS_FranGV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            APIFichero.APIFichero.EscribirFichero("Hollaaaa");
+
+            APIFichero.APIFichero.EscribirFichero("Adssios");
+
+        }
+    }
+}
