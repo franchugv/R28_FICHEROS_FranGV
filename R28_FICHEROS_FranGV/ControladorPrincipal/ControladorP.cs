f74e40261da66fc31c67421cb1c90c75ced13640
DAM 1 <DAM 1@DESKTOP-TKRSI9T>
2024-04-10 13:06:45 +0200

    adssadad

---
+++ R28_FICHEROS_FranGV/ControladorPrincipal/ControladorP.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R28_FICHEROS_FranGV.ControladorPrincipal
+{
+    public enum OpcionesPrincipal : byte { Salir, Añadir, Consultar }
+    public static class ControladorP
+    {
+        
+        public static void ControladorPrincipal()
+        {
+            // Recursos
+            bool esValido;
+            OpcionesPrincipal opcion = OpcionesPrincipal.Salir;
+            string mError = "";
+
+            do
+            {
+                esValido = true;
+                try
+                {
+
+                }
+                catch (Exception error)
+                {
+                    esValido = false;
+                    mError = error.Message;
+                }
+                finally
+                {
+                    if(!esValido) 
+                }
+
+            } while (!esValido || opcion != OpcionesPrincipal.Salir);
+
+            
+        }
+    }
+}
--- R28_FICHEROS_FranGV/UIPrincipal/UIPrincipal.cs
+++ R28_FICHEROS_FranGV/UIPrincipal/UIPrincipal.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace R28_FICHEROS_FranGV.UIPrincipal
 {
-    internal class UIPrincipal
+    public static class UIPrincipal
     {
+        public static void UIP()
+        {
+            Console.WriteLine("\t-MENÚ PRINCIPAL-\n");
+            Console.WriteLine("\t\t0. Salír.");
+            Console.WriteLine("\t\t1. Añadir.");
+            Console.WriteLine("\t\t2. Consultar.");
+            Console.Write("\nElija una opción: ");
+
+        }
     }
 }
